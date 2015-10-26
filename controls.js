a471b454813da95bd6cfccc8f4fb73c669fd79b9
Mike Vattuone <mvattuone@gmail.com>
2015-10-26 14:03:20 -0400

    trying to do things in a more modular manner -- include browserify, some refactoring

--- controls.js
+++ controls.js
@@ -6,124 +6,130 @@
  * Edited to include Raycasting and sound toggle by mvattuone
  */
 
-THREE.VattuonetControls = function(object) {
-    var controls = this.initialize(object);
-    return controls;
-}
+module.exports = function(THREE) {
 
-THREE.VattuonetControls.prototype.initialize = function(object) { 
-    var scope = this;
-    scope.PI_2 = Math.PI / 2;
-    scope.mouseQuat = {
-      x: new THREE.Quaternion(),
-      y: new THREE.Quaternion()
-    };
-    scope.object = object;
-    scope.xVector = new THREE.Vector3( 1, 0, 0 );
-    scope.yVector = new THREE.Vector3( 0, 1, 0 );
-    scope.enabled = true;
-    scope.orientation = {
-        x: 0,
-        y: 0,
-    };
 
-  document.addEventListener('mousemove', scope.onMouseMove, false );
-  // We use debounce to ensure songs are only triggered once (i.e. when there is an intersection)
-  document.addEventListener('mousemove', _.debounce(function(event) { scope.checkPosition(event) }, 200), false );
+  function VattuonetControls(object) {
+    
+      this.initialize = function(object) { 
+        var scope = this;
+        scope.PI_2 = Math.PI / 2;
+        scope.mouseQuat = {
+          x: new THREE.Quaternion(),
+          y: new THREE.Quaternion()
+        };
+        scope.object = object;
+        scope.xVector = new THREE.Vector3( 1, 0, 0 );
+        scope.yVector = new THREE.Vector3( 0, 1, 0 );
+        scope.enabled = true;
+        scope.orientation = {
+            x: 0,
+            y: 0,
+        };
 
-  return scope;
-}
+        document.addEventListener('mousemove', scope.onMouseMove, false );
+        // We use debounce to ensure songs are only triggered once (i.e. when there is an intersection)
+        document.addEventListener('mousemove', _.debounce(function(event) { scope.checkPosition(event) }, 200), false );
 
-THREE.VattuonetControls.prototype.onMouseMove = function ( event ) {
-    if ( app.camControls.enabled === false ) return;
+        return scope;
+      };
 
-    var movementX = event.movementX || 0,
-        movementY = event.movementY || 0;
+      this.onMouseMove = function ( event ) {
+          if ( app.camControls.enabled === false ) return;
 
-    app.camControls.orientation.y -= movementX * 0.0025;
-    app.camControls.orientation.x -= movementY * 0.0025;
-    app.camControls.orientation.x = Math.max(-app.camControls.PI_2, Math.min(app.camControls.PI_2, app.camControls.orientation.x));
-};
+          var movementX = event.movementX || 0,
+              movementY = event.movementY || 0;
 
-THREE.VattuonetControls.prototype.getIntersection = function(event) {
-  app.mouse.x = ( event.clientX / app.renderer.domElement.width ) * 2 - 1;
-  app.mouse.y = - ( event.clientY / app.renderer.domElement.height ) * 2 + 1;
+          app.camControls.orientation.y -= movementX * 0.0025;
+          app.camControls.orientation.x -= movementY * 0.0025;
+          app.camControls.orientation.x = Math.max(-app.camControls.PI_2, Math.min(app.camControls.PI_2, app.camControls.orientation.x));
+      };
 
-  app.raycaster.setFromCamera( app.mouse, app.camera );
-  intersects = app.raycaster.intersectObjects(app.spheres);
-  if (intersects.length <= 0) {
-    return false;
-  }
-  return intersects;
-};
+      this.getIntersection = function(event) {
+        app.mouse.x = ( event.clientX / app.renderer.domElement.width ) * 2 - 1;
+        app.mouse.y = - ( event.clientY / app.renderer.domElement.height ) * 2 + 1;
 
-THREE.VattuonetControls.prototype.checkPosition = function(event) {
-  app.intersects = app.camControls.getIntersection(event);
+        app.raycaster.setFromCamera( app.mouse, app.camera );
+        intersects = app.raycaster.intersectObjects(app.spheres);
+        if (intersects.length <= 0) {
+          return false;
+        }
+        return intersects;
+      };
 
-  if (!app.intersects) { 
-    if (app.sound) { app.sound.source.stop(); }
-    app.currentSong = null;
-    return false; 
-  }
+      this.checkPosition = function(event) {
+        app.intersects = app.camControls.getIntersection(event);
 
-  if (!app.audioCtx) {
-      app.audioCtx = new AudioContext();
-      var song;
-  } 
+        if (!app.intersects) { 
+          if (app.sound) { app.sound.source.stop(); }
+          app.currentSong = null;
+          return false; 
+        }
 
-  if (app.intersects[ 0 ].object.id === 8) {
-    song = 1;
-  }
-  else if (app.intersects[ 0 ].object.id === 10) {
-    song = 2;
-  }
-  else if (app.intersects[ 0 ].object.id === 12) {
-    song = 3;
-  }
-  else if (app.intersects[ 0 ].object.id === 14) {
-    song = 4;
-  } 
+        if (!app.audioCtx) {
+            app.audioCtx = new AudioContext();
+            var song;
+        } 
 
-  if (app.currentSong === song) {
-    return false;
-  }
+        if (app.intersects[ 0 ].object.id === 8) {
+          song = 1;
+        }
+        else if (app.intersects[ 0 ].object.id === 10) {
+          song = 2;
+        }
+        else if (app.intersects[ 0 ].object.id === 12) {
+          song = 3;
+        }
+        else if (app.intersects[ 0 ].object.id === 14) {
+          song = 4;
+        } 
 
-  app.sound = {};
-  app.sound.source = app.audioCtx.createBufferSource();
-  app.sound.volume = app.audioCtx.createGain();
-  app.sound.source.loop = true;
+        if (app.currentSong === song) {
+          return false;
+        }
 
-  // Load a app.sound file using an ArrayBuffer XMLHttpRequest.
-  var request = new XMLHttpRequest();
-  request.open("GET", 'vattuonet-' + song + '.mp3', true);
-  request.responseType = "arraybuffer";
-  request.onload = function(e) {
-    // Create a buffer from the response ArrayBuffer.
-    app.audioCtx.decodeAudioData(request.response, function onSuccess(buffer) {
+        app.sound = {};
+        app.sound.source = app.audioCtx.createBufferSource();
+        app.sound.volume = app.audioCtx.createGain();
+        app.sound.source.loop = true;
 
-      // Make the app.sound source use the buffer and start playing it.
-      if (!app.sound.source.buffer) {
-        app.sound.source.buffer = buffer;
-      } else {
-        app.sound.source = app.audioCtx.createBufferSource(); 
-        app.sound.source.buffer = buffer;
+        // Load a app.sound file using an ArrayBuffer XMLHttpRequest.
+        var request = new XMLHttpRequest();
+        request.open("GET", 'vattuonet-' + song + '.mp3', true);
+        request.responseType = "arraybuffer";
+        request.onload = function(e) {
+          // Create a buffer from the response ArrayBuffer.
+          app.audioCtx.decodeAudioData(request.response, function onSuccess(buffer) {
+
+            // Make the app.sound source use the buffer and start playing it.
+            if (!app.sound.source.buffer) {
+              app.sound.source.buffer = buffer;
+            } else {
+              app.sound.source = app.audioCtx.createBufferSource(); 
+              app.sound.source.buffer = buffer;
+            }
+            app.sound.source.connect(app.audioCtx.destination);
+            app.sound.source.start(app.audioCtx.currentTime);
+            app.currentSong = song;
+          }, function onFailure() {
+            alert("Decoding the audio buffer failed");
+          });
+        };
+        request.send();
+      };
+
+      this.update = function() {
+          if ( app.camControls.enabled === false ) return;
+
+          app.camControls.mouseQuat.x.setFromAxisAngle( app.camControls.xVector, app.camControls.orientation.x );
+          app.camControls.mouseQuat.y.setFromAxisAngle( app.camControls.yVector, app.camControls.orientation.y );
+          app.camControls.object.quaternion.copy( app.camControls.mouseQuat.y ).multiply( app.camControls.mouseQuat.x );
+          return;
       }
-      app.sound.source.connect(app.audioCtx.destination);
-      app.sound.source.start(app.audioCtx.currentTime);
-      app.currentSong = song;
-    }, function onFailure() {
-      alert("Decoding the audio buffer failed");
-    });
-  };
-  request.send();
-};
+    
+    }
 
-THREE.VattuonetControls.prototype.update = function() {
-    if ( app.camControls.enabled === false ) return;
-
-    app.camControls.mouseQuat.x.setFromAxisAngle( app.camControls.xVector, app.camControls.orientation.x );
-    app.camControls.mouseQuat.y.setFromAxisAngle( app.camControls.yVector, app.camControls.orientation.y );
-    app.camControls.object.quaternion.copy( app.camControls.mouseQuat.y ).multiply( app.camControls.mouseQuat.x );
-    return;
+    VattuonetControls.prototype = Object.create(THREE.EventDispatcher.prototype);
+    VattuonetControls.prototype.constructor = VattuonetControls;
+    return VattuonetControls;
 }
-
--- main.js
+++ main.js
@@ -1,3 +1,337 @@
+// Imports 
+
+$ = require('jquery');
+_ = require('underscore');
+THREE = require('threejs-build');
+VattuonetControls = require('./controls')(THREE);
+helvetiker = require('three.regular.helvetiker');
+Spinner = require('spin');
+
+
+/* StackBlur - a fast almost Gaussian Blur For Canvas
+
+Version:    0.5
+Author:     Mario Klingemann
+Contact:    mario@quasimondo.com
+Website:    http://www.quasimondo.com/StackBlurForCanvas
+Twitter:    @quasimondo
+
+In case you find this class useful - especially in commercial projects -
+I am not totally unhappy for a small donation to my PayPal account
+mario@quasimondo.de
+
+Or support me on flattr: 
+https://flattr.com/thing/72791/StackBlur-a-fast-almost-Gaussian-Blur-Effect-for-CanvasJavascript
+
+Copyright (c) 2010 Mario Klingemann
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+function stackBlurCanvasRGB( id, top_x, top_y, width, height, radius ) {
+
+  var mul_table = [
+    512,512,456,512,328,456,335,512,405,328,271,456,388,335,292,512,
+    454,405,364,328,298,271,496,456,420,388,360,335,312,292,273,512,
+    482,454,428,405,383,364,345,328,312,298,284,271,259,496,475,456,
+    437,420,404,388,374,360,347,335,323,312,302,292,282,273,265,512,
+    497,482,468,454,441,428,417,405,394,383,373,364,354,345,337,328,
+    320,312,305,298,291,284,278,271,265,259,507,496,485,475,465,456,
+    446,437,428,420,412,404,396,388,381,374,367,360,354,347,341,335,
+    329,323,318,312,307,302,297,292,287,282,278,273,269,265,261,512,
+    505,497,489,482,475,468,461,454,447,441,435,428,422,417,411,405,
+    399,394,389,383,378,373,368,364,359,354,350,345,341,337,332,328,
+    324,320,316,312,309,305,301,298,294,291,287,284,281,278,274,271,
+    268,265,262,259,257,507,501,496,491,485,480,475,470,465,460,456,
+    451,446,442,437,433,428,424,420,416,412,408,404,400,396,392,388,
+    385,381,377,374,370,367,363,360,357,354,350,347,344,341,338,335,
+    332,329,326,323,320,318,315,312,310,307,304,302,299,297,294,292,
+    289,287,285,282,280,278,275,273,271,269,267,265,263,261,259 
+  ],
+  shg_table = [
+    9, 11, 12, 13, 13, 14, 14, 15, 15, 15, 15, 16, 16, 16, 16, 17, 
+    17, 17, 17, 17, 17, 17, 18, 18, 18, 18, 18, 18, 18, 18, 18, 19, 
+    19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 19, 20, 20, 20,
+    20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 20, 21,
+    21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 21,
+    21, 21, 21, 21, 21, 21, 21, 21, 21, 21, 22, 22, 22, 22, 22, 22, 
+    22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22,
+    22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 22, 23, 
+    23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+    23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23,
+    23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 23, 
+    23, 23, 23, 23, 23, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
+    24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24 
+  ];
+
+  function BlurStack() {
+    this.r = 0;
+    this.g = 0;
+    this.b = 0;
+    this.a = 0;
+    this.next = null;
+  }
+
+  if ( isNaN(radius) || radius < 1 ) return;
+  radius |= 0;
+  
+  if (app.canvas) {
+    var canvas = app.canvas;
+  } else {
+    var canvas  = document.getElementById( id );  
+  }
+  
+  var context = canvas.getContext("2d");
+  var imageData;
+  
+  try {
+    imageData = context.getImageData( top_x, top_y, width, height );
+  } catch(e) {
+    alert("Cannot access image");
+    throw new Error("unable to access image data: " + e);
+  }
+
+  var pixels = imageData.data;
+
+  var x, y, i, p, yp, yi, yw, r_sum, g_sum, b_sum,
+  r_out_sum, g_out_sum, b_out_sum,
+  r_in_sum, g_in_sum, b_in_sum,
+  pr, pg, pb, rbs;
+
+  var div = radius + radius + 1,
+      w4 = width << 2,
+      widthMinus1  = width - 1,
+      heightMinus1 = height - 1,
+      radiusPlus1  = radius + 1,
+      sumFactor = radiusPlus1 * ( radiusPlus1 + 1 ) / 2;
+  
+  var stackStart = new BlurStack(),
+      stack = stackStart;
+
+  for ( i = 1; i < div; i++ ) {
+    stack = stack.next = new BlurStack();
+    if ( i == radiusPlus1 ) var stackEnd = stack;
+  }
+
+  stack.next = stackStart;
+  
+  var stackIn = null,
+      stackOut = null;
+  
+  yw = yi = 0;
+  
+  var mul_sum = mul_table[radius],
+      shg_sum = shg_table[radius];
+  
+  for ( y = 0; y < height; y++ ) {
+    r_in_sum = g_in_sum = b_in_sum = r_sum = g_sum = b_sum = 0;
+
+    r_out_sum = radiusPlus1 * ( pr = pixels[yi] );
+    g_out_sum = radiusPlus1 * ( pg = pixels[yi+1] );
+    b_out_sum = radiusPlus1 * ( pb = pixels[yi+2] );
+
+    r_sum += sumFactor * pr;
+    g_sum += sumFactor * pg;
+    b_sum += sumFactor * pb;
+
+    stack = stackStart;
+
+    for( i = 0; i < radiusPlus1; i++ ) {
+      stack.r = pr;
+      stack.g = pg;
+      stack.b = pb;
+      stack = stack.next;
+    }
+
+    for( i = 1; i < radiusPlus1; i++ ) {
+      p = yi + (( widthMinus1 < i ? widthMinus1 : i ) << 2 );
+      r_sum += ( stack.r = ( pr = pixels[p])) * ( rbs = radiusPlus1 - i );
+      g_sum += ( stack.g = ( pg = pixels[p+1])) * rbs;
+      b_sum += ( stack.b = ( pb = pixels[p+2])) * rbs;
+
+      r_in_sum += pr;
+      g_in_sum += pg;
+      b_in_sum += pb;
+
+      stack = stack.next;
+    }
+
+
+    stackIn = stackStart;
+    stackOut = stackEnd;
+    for ( x = 0; x < width; x++ ) {
+      pixels[yi]   = (r_sum * mul_sum) >> shg_sum;
+      pixels[yi+1] = (g_sum * mul_sum) >> shg_sum;
+      pixels[yi+2] = (b_sum * mul_sum) >> shg_sum;
+
+      r_sum -= r_out_sum;
+      g_sum -= g_out_sum;
+      b_sum -= b_out_sum;
+
+      r_out_sum -= stackIn.r;
+      g_out_sum -= stackIn.g;
+      b_out_sum -= stackIn.b;
+
+      p =  ( yw + ( ( p = x + radius + 1 ) < widthMinus1 ? p : widthMinus1 ) ) << 2;
+
+      r_in_sum += ( stackIn.r = pixels[p]);
+      g_in_sum += ( stackIn.g = pixels[p+1]);
+      b_in_sum += ( stackIn.b = pixels[p+2]);
+
+      r_sum += r_in_sum;
+      g_sum += g_in_sum;
+      b_sum += b_in_sum;
+
+      stackIn = stackIn.next;
+
+      r_out_sum += ( pr = stackOut.r );
+      g_out_sum += ( pg = stackOut.g );
+      b_out_sum += ( pb = stackOut.b );
+
+      r_in_sum -= pr;
+      g_in_sum -= pg;
+      b_in_sum -= pb;
+
+      stackOut = stackOut.next;
+
+      yi += 4;
+    }
+    yw += width;
+  }
+
+  
+  for ( x = 0; x < width; x++ ) {
+    g_in_sum = b_in_sum = r_in_sum = g_sum = b_sum = r_sum = 0;
+
+    yi = x << 2;
+    r_out_sum = radiusPlus1 * ( pr = pixels[yi]);
+    g_out_sum = radiusPlus1 * ( pg = pixels[yi+1]);
+    b_out_sum = radiusPlus1 * ( pb = pixels[yi+2]);
+
+    r_sum += sumFactor * pr;
+    g_sum += sumFactor * pg;
+    b_sum += sumFactor * pb;
+
+    stack = stackStart;
+
+    for( i = 0; i < radiusPlus1; i++ ) {
+      stack.r = pr;
+      stack.g = pg;
+      stack.b = pb;
+      stack = stack.next;
+    }
+
+    yp = width;
+
+    for( i = 1; i <= radius; i++ ) {
+      yi = ( yp + x ) << 2;
+
+      r_sum += ( stack.r = ( pr = pixels[yi])) * ( rbs = radiusPlus1 - i );
+      g_sum += ( stack.g = ( pg = pixels[yi+1])) * rbs;
+      b_sum += ( stack.b = ( pb = pixels[yi+2])) * rbs;
+
+      r_in_sum += pr;
+      g_in_sum += pg;
+      b_in_sum += pb;
+
+      stack = stack.next;
+      
+      if( i < heightMinus1 ) {
+        yp += width;
+      }
+    }
+
+    yi = x;
+    stackIn = stackStart;
+    stackOut = stackEnd;
+    for ( y = 0; y < height; y++ ) {
+      p = yi << 2;
+      pixels[p]   = (r_sum * mul_sum) >> shg_sum;
+      pixels[p+1] = (g_sum * mul_sum) >> shg_sum;
+      pixels[p+2] = (b_sum * mul_sum) >> shg_sum;
+
+      r_sum -= r_out_sum;
+      g_sum -= g_out_sum;
+      b_sum -= b_out_sum;
+
+      r_out_sum -= stackIn.r;
+      g_out_sum -= stackIn.g;
+      b_out_sum -= stackIn.b;
+
+      p = ( x + (( ( p = y + radiusPlus1) < heightMinus1 ? p : heightMinus1 ) * width )) << 2;
+
+      r_sum += ( r_in_sum += ( stackIn.r = pixels[p]));
+      g_sum += ( g_in_sum += ( stackIn.g = pixels[p+1]));
+      b_sum += ( b_in_sum += ( stackIn.b = pixels[p+2]));
+
+      stackIn = stackIn.next;
+
+      r_out_sum += ( pr = stackOut.r );
+      g_out_sum += ( pg = stackOut.g );
+      b_out_sum += ( pb = stackOut.b );
+
+      r_in_sum -= pr;
+      g_in_sum -= pg;
+      b_in_sum -= pb;
+
+      stackOut = stackOut.next;
+
+      yi += width;
+    }
+  }
+  
+  context.putImageData( imageData, top_x, top_y ); }
+
+THREE.Object3D.prototype.GdeepCloneMaterials = function() {
+  var object = this.clone( new THREE.Object3D(), false );
+
+  for ( var i = 0; i < this.children.length; i++ ) {
+
+    var child = this.children[ i ];
+    if ( child.GdeepCloneMaterials ) {
+      object.add( child.GdeepCloneMaterials() );
+    } else {
+      object.add( child.clone() );
+    }
+
+  }
+  return object;};
+
+THREE.Mesh.prototype.GdeepCloneMaterials = function( object, recursive ) {
+  if ( object === undefined ) {
+    object = new THREE.Mesh( this.geometry, this.material.clone() );
+  }
+
+  THREE.Object3D.prototype.GdeepCloneMaterials.call( this, object, recursive );
+
+  return object;
+};
+
+/* END PLUGINS */
+
+
 /**
  * Populates blog with posts that are not tagged (i.e. page content) returned from the Tumblr API
  * @return the blog panel populated with posts
@@ -88,9 +422,10 @@ createSpinner = function() {
 revealScene = function(event) { 
   app.spinner.stop();
   $('body').addClass('loaded');
+
 }
 
-draw = function(type,v,c,w,h,first) {
+draw = function(type,v,c,w,h) {
   
   if(v.paused || v.ended) return false;
 
@@ -138,10 +473,6 @@ draw = function(type,v,c,w,h,first) {
       draw("normal", app.video,app.context,cw,ch);
     }
   }, 0);
-
-  if (first) {
-    
-  }
 };
 
 greyScaleDraw = function(data) {
@@ -171,35 +502,6 @@ embossDraw = function(data, idata) {
   return data;
 };
 
-successCallback = function(stream) {
-  if (app.video.mozSrcObject !== undefined) {
-    app.video.mozSrcObject = stream;
-  } else {
-    app.video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
-  };
-    
-  app.video.onplay = function() {
-    cw = app.video.clientWidth;
-    ch = app.video.clientHeight;
-    app.canvas.width = cw;
-    app.canvas.height = ch;
-    draw("greyScale",app.video,app.context,cw,ch,true);
-  };
-
-  app.video.onloadeddata = function() {
-    revealScene();
-    app.video.play();
-  };
-
-  app.video.load();
-  
-};
-
-// The error callback is also triggered when the camera is not active...
-errorCallback = function(error) {
-  console.error('An error occurred: [CODE ' + error.code + ']');
-};
-
 onDocumentTouchStart = function(event) {
   event.preventDefault();
   event.clientX = event.touches[0].clientX;
@@ -209,10 +511,8 @@ onDocumentTouchStart = function(event) {
 
 onDocumentMouseDown = function(event) {
   event.preventDefault();
-  alert('foo');
 
   var intersects = app.camControls.getIntersection(event);
-  alert(intersects);
   if (!intersects) { return false; }
 
   var route;
@@ -247,6 +547,7 @@ buildSphere = function(radius,widthSegments,heightSegments) {
 
 /* Give a more human readable name to each sphere, using the Mesh ID provided upon building the mesh. */
 assignLabel = function(meshId) {
+  console.log(meshId);
   var label;
   if (meshId === 8) {
     label = "blog";
@@ -270,7 +571,7 @@ labelSphere = function(label) {
   })
 
   var textGeo = new THREE.TextGeometry(label.toUpperCase(), {
-    font: 'droid sans',
+    font: 'helvetiker',
     weight: 'normal',
     style: 'normal',
     size: '14',
@@ -286,45 +587,58 @@ setUIEventHandlers = function() {
   $('.panel').on('transitionend', sanitizePanel);
 };
 
-init = function() {    
+initCamera = function() {
+
+  cameraSuccess = function(stream) {
+    if (app.video.mozSrcObject !== undefined) {
+      app.video.mozSrcObject = stream;
+    } else {
+      app.video.src = (window.URL && window.URL.createObjectURL(stream)) || stream;
+    };
+      
+    app.video.onplay = function() {
+      cw = app.video.clientWidth;
+      ch = app.video.clientHeight;
+      app.canvas.width = cw;
+      app.canvas.height = ch;
+      draw("greyScale",app.video,app.context,cw,ch);
+    };
+
+    app.video.onloadeddata = function() {
+      revealScene();
+      app.video.play();
+    };
+
+    app.video.load();
+    
+  };
+
+  // The error callback is also triggered when the camera is not active...
+  cameraError = function(error) {
+    console.error('An error occurred: [CODE ' + error.code + ']');
+  };
+
+  app.video = $('#video')[0];
   navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
   window.URL = window.URL || window.webkitURL || window.mozURL || window.msURL;
   if (navigator.getUserMedia) {
-    navigator.getUserMedia({video: true}, successCallback, errorCallback);
+    navigator.getUserMedia({video: true}, cameraSuccess, cameraError);
   } else {
     throw 'getUserMedia Error: Native device media streaming (getUserMedia) not supported in this browser.';
   }
+};
 
-  // Detect if the audio context is supported.
+initAudio = function() { 
+  // Web Audio API stuff
   window.AudioContext = ( window.AudioContext || window.webkitAudioContext || null );
 
   if (!AudioContext) { 
     // fallback
   } 
+};
 
-  app.scene = new THREE.Scene();
-  app.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 100000);
-
-  // for clicking on THREE objects
-  app.raycaster = new THREE.Raycaster();
-  app.mouse = new THREE.Vector3();
-
-  // for moving around the scene
-  app.clock = new THREE.Clock();
-
-  app.renderer = new THREE.WebGLRenderer({ alpha:true });
-  app.renderer.domElement.id = "scene";
-  app.renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( app.renderer.domElement );
-
-  app.video = $('#video')[0];
-
-  app.canvas = document.createElement('canvas');
-  app.canvas.id = "canvas";
-  app.context = app.canvas.getContext('2d');
-
-  var cw = app.canvas.width = 1024,
-      ch = app.canvas.height = 1024;
+THREE.Scene.prototype.addLighting = function() { 
+  a = []
 
   var ambientLight = new THREE.AmbientLight( 0x000000 );
   app.scene.add( ambientLight );
@@ -339,7 +653,41 @@ init = function() {
   app.scene.add(lights[0]);
   app.scene.add(lights[1]);
   app.scene.add(lights[2]);
-  app.texture = new THREE.Texture(app.canvas);
+}
+
+initScene = function() { 
+  app.scene = new THREE.Scene();
+  app.camera = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 100000);
+  app.raycaster = new THREE.Raycaster(); // used with intersections
+  app.mouse = new THREE.Vector3(); // used with intersections
+  app.clock = new THREE.Clock(); // used with controls
+  app.renderer = new THREE.WebGLRenderer({ alpha:true });
+  app.renderer.domElement.id = "scene";
+  app.renderer.setSize( window.innerWidth, window.innerHeight );
+  document.body.appendChild( app.renderer.domElement );
+  app.scene.addLighting();
+};
+
+init = function() {    
+
+  // Webcam stuff
+  initCamera();
+  initAudio();
+  initScene();
+  
+
+  buildCameraTexture = function() { 
+    app.canvas = document.createElement('canvas');
+    app.canvas.id = "canvas";
+    app.context = app.canvas.getContext('2d');
+
+    var cw = app.canvas.width = 1024,
+        ch = app.canvas.height = 1024;
+
+    return new THREE.Texture(app.canvas);
+  }
+
+  app.texture = buildCameraTexture();  
 
   var cube = new THREE.Mesh( 
     new THREE.BoxGeometry(100000,100000,100000,1,1,1, null, true), 
@@ -398,7 +746,8 @@ init = function() {
     app.scene.add(label);
   });
 
-  app.camControls = new THREE.VattuonetControls(app.camera);
+  app.camControls = new VattuonetControls(app.camera);
+  app.camControls.initialize(app.camera);
   app.camControls.enableDamping = true;
   app.camControls.dampingFactor = 0.25;
   app.camControls.enableZoom = false;
@@ -411,6 +760,7 @@ init = function() {
   render();
 }
 
+THREE.typeface_js.loadFace(helvetiker);
 createSpinner(); // So we don't see DOM weirdness
 
 $(document).ready(function() {
